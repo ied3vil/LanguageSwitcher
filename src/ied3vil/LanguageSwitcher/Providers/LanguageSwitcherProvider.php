655e07b94188b307f3a03062846c2437e0806a22
IeD3vil <ied3vil@gmail.com>
2016-07-16 14:22:49 +0100

    Added some functionality

--- src/ied3vil/LanguageSwitcher/Providers/LanguageSwitcherProvider.php
+++ src/ied3vil/LanguageSwitcher/Providers/LanguageSwitcherProvider.php
@@ -8,11 +8,11 @@ class LanguageSwitcherProvider extends ServiceProvider
 {
     public function register()
     {
-
+        echo 'Language Provider Registered';
     }
 
     public function boot()
     {
-        
+        echo 'Language Provider Booted';
     }
 }
