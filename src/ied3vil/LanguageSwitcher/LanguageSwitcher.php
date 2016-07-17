094fbe2c71b085dd252c90bd2ae8a0b4e8bc5427
IeD3vil <ied3vil@gmail.com>
2016-07-17 10:55:35 +0100

    Added docblocks

--- src/ied3vil/LanguageSwitcher/LanguageSwitcher.php
+++ src/ied3vil/LanguageSwitcher/LanguageSwitcher.php
@@ -8,12 +8,18 @@ use Illuminate\Support\Facades\Request;
 
 class LanguageSwitcher
 {
-
+    /**
+     * Registers the language to the app Locale
+     */
     public function registerLanguage()
     {
         App::setLocale($this->getCurrentLanguage());
     }
 
+    /**
+     * Returns the current language set in the session/cookie
+     * @return string
+     */
     public function getCurrentLanguage()
     {
         if ($this->getStorageMethod() == 'session') {
@@ -22,6 +28,11 @@ class LanguageSwitcher
         return Request::cookie($this->getLanguageKey());
     }
 
+    /**
+     * Sets the language flag and returns the cookie to be created on the redirect
+     * @param $language
+     * @return mixed | null
+     */
     public function setLanguage($language)
     {
         if ($this->getStorageMethod() == 'cookie') {
@@ -31,25 +42,46 @@ class LanguageSwitcher
         $this->registerLanguage();
     }
 
+    /**
+     * Gets the storage method for the language flag
+     * @return string session | cookie
+     */
     public function getStorageMethod()
     {
         return Config::get('languageswitcher.store', 'session'); //defaults to session
     }
 
+    /**
+     * Gets the language key that is used to store the language
+     * @return mixed
+     */
     public function getLanguageKey()
     {
         return Config::get('languageswitcher.key', 'language');
     }
 
+    /**
+     * Gets the switch route for switching the language
+     * @return string
+     */
     public function getSwitchPath()
     {
         return Config::get('languageswitcher.switchPath', 'lang');
     }
 
+    /**
+     * Gets the redirect type, that can be route or back
+     * @return string
+     */
     public function getRedirect()
     {
-        return Config::get('languageswitcher.redirect', 'redirect');
+        return Config::get('languageswitcher.redirect', 'route');
     }
+
+    /**
+     * Gets the redirect route set in the config
+     * @return string
+     */
     public function getRedirectRoute()
     {
         return Config::get('languageswitcher.redirect_route', '/');
